e84bc635732920368b7d54718e738f5d558fd70f
fabiochidda <fabio.chidda99@gmail.com>
2022-05-08 23:47:55 +0200

    Aggiunto category_id random con faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Post;
+use App\Category;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -15,6 +16,10 @@ class PostSeeder extends Seeder
     public function run(Faker $faker)
     {
 
+        $categories = Category::all();
+
+        $categoriesId = $categories->pluck('id')->all();
+
         for ($i=0; $i<100; $i++) {
 
             $post = new Post();
@@ -23,6 +28,7 @@ class PostSeeder extends Seeder
             $post->slug = Str::slug($post->title);
             $post->content = $faker->paragraphs(10, true);
             $post->published_at = $faker->dateTime();
+            $post->category_id = $faker->optional()->randomElement($categoriesId);
 
             $post->save();
 
