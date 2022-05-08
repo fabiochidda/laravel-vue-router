b09d686a572edcf24c4b9297a73b188e993759b4
fabiochidda <fabio.chidda99@gmail.com>
2022-05-08 13:52:49 +0200

    Aggiunta la tabella categories ed inseriti i dati

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +13,21 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            'Antipasti',
+            'Primi',
+            'Secondi',
+            'Contorni',
+            'Dolci'
+        ];
+
+        foreach($categories as $name) {
+
+            $category = new Category();
+            $category->name = $name;
+            $category->slug = Str::slug($name);
+
+            $category->save();
+        }
     }
 }
